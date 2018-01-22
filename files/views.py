b9e21eb5c6237b5df68b571ff48a7461906ff623
taqpol <njosephchen@gmail.com>
2018-01-21 18:09:51 -0800

    added image model

--- files/views.py
+++ files/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import permissions, status, authentication
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+class FileUploadView(APIView):
+	def post(self, request, *args, **kwargs):
+		pass
+
