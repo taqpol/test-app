8a0f9657d133ffd9165041a57d117c3d903984b7
taqpol <njosephchen@gmail.com>
2018-02-26 23:14:12 -0800

    getting api browser to work

--- slideshow/urls.py
+++ slideshow/urls.py
@@ -4,9 +4,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
 
+import files.urls
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls')),
+    url(r'^files/', include('files.urls')),
     # url(r'^api-token-auth/', obtain_jwt_token),
     # url(r'^api-token-refresh/', refresh_jwt_token),
 ]
